69c8859ef1e5b77b7fc842b7257d2736d12118a5
nicholas <nicholaspheld@gmail.com>
2021-03-05 10:19:08 -0600

    updated JS with hiding the instructions and start button upon click, started working on timer counter

--- README.md
+++ README.md
@@ -2,6 +2,9 @@ This is a timed coding quiz with multiple-choice questions about Javascript basi
 This app will run in the browser and will feature dynamically 
 updated HTML and CSS powered by JavaScript code. 
 
+There are six multiple choice questions. The user is given 120 seconds to complete and the timer will start the countdown once the start button is clicked. The quiz will be ended early if the timer expires before finishing. 
+
+When the user finishes the quiz or the timer ends, the user can enter their initials and score.
 
 gethub link:
 
--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 //jss
+var timer = 120
+var timerStart = document.querySelector("#start");
+var countE1 = document.querySelector(".count");
+//submitE1 = document.querySelector(#submit); submit final results with initials
+timerStart.addEventListener("click", function () {
+
+    instructions.style.visibility = "hidden";
+    start.style.visibility = "hidden";
+
+    countDown();
+});
+var countDown = function (timer) {
+
+    for (i = timer; i > 0; i--) {
+        countDown(timer);
+        console.log(i);
+        countE1.textContent = count;
+    }
+};
+
+//submitE1.addEventListener("click, initials")
+
+/*
+1C From the given array which index is the letter 'b' on? ['a', 'b', 'c', 'd'] *
+3
+2
+1
+0
+
+2A What are the six primitive data types in JavaScript?
+string, number, boolean, bigInt, symbol, undefined
+sentence, float, data, bigInt, symbol, undefined
+sentence, int, truthy, bigInt, symbol, undefined
+string, num, falsy, bigInt, symbol, undefined
+
+3D  How do we declare a conditional statement in JavaScript? *
+difference...between
+for loop
+while loop
+if...else
+
+4C How do we stop a loop from from repeating indefinitely? *
+When we have iterated through half of the condition.
+We have to explicitly end the loop with the break keyword.
+A loop will stop executing when the condition is false.
+A loop will stop executing when the condition is true.
+
+5B As a developer, I want to be able to remove the last element of my array and I want to also be able to add a new element to the beginning of my array. Which two array methods should I use? *
+concat() and shift()
+pop() and unshift()
+push() and sort()
+forEach() and pop()
+
+6D Inside the HTML document, where do you place your JavaScript code? *
+In the <footer> element
+Inside the <head> element
+Inside the <link> element
+Inside the <script> element
+*/
